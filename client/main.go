923508ff9b5463d1df6b63257cffbe6fcb387231
tensor-programming <abeltensor@tensor-programming.com>
2019-02-06 18:40:07 -0500

    first commit

---
+++ client/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"grpc_tutorial/proto"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost:4040", grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+
+	client := proto.NewAddServiceClient(conn)
+
+	g := gin.Default()
+	g.GET("/add/:a/:b", func(ctx *gin.Context) {
+		a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
+			return
+		}
+
+		b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
+			return
+		}
+
+		req := &proto.Request{A: int64(a), B: int64(b)}
+		if response, err := client.Add(ctx, req); err == nil {
+			ctx.JSON(http.StatusOK, gin.H{
+				"result": fmt.Sprint(response.Result),
+			})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+	})
+
+	g.GET("/mult/:a/:b", func(ctx *gin.Context) {
+		a, err := strconv.ParseUint(ctx.Param("a"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter A"})
+			return
+		}
+		b, err := strconv.ParseUint(ctx.Param("b"), 10, 64)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Parameter B"})
+			return
+		}
+		req := &proto.Request{A: int64(a), B: int64(b)}
+
+		if response, err := client.Multiply(ctx, req); err == nil {
+			ctx.JSON(http.StatusOK, gin.H{
+				"result": fmt.Sprint(response.Result),
+			})
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+	})
+
+	if err := g.Run(":8080"); err != nil {
+		log.Fatalf("Failed to run server: %v", err)
+	}
+
+}
